1f743b184fb9ba191be30d12dad72f7f47cc4f7a
Peter Naydenov <peter.naydenov@gmail.com>
2022-11-17 11:49:31 +0200

    Package update

--- src/main.js
+++ src/main.js
@@ -38,6 +38,8 @@ function deepCopy ( origin ) {
 
 
 function findType ( x ) {
+    if ( x == null              )   return 'simple'
+    if ( typeof x === 'boolean' )   return 'simple'
     if ( x instanceof Array     ) return 'array'
     if ( typeof x === 'object'  ) return 'object'
     return 'simple'
